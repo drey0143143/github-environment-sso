505ba9cc7b748bc029647ec5dc08ccb575bd677d
oludare odo <darekorex143@gmail.com>
2022-08-04 08:42:42 -0700

    initial commit

--- module/tf-code/main.tf
+++ module/tf-code/main.tf
@@ -46,10 +46,10 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
-  instance_type = "t100.micro"
+  instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = HelloWorld
   }
 }
 
